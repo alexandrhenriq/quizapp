ee04d3d1fa0245b99c937d281f229be4f372edf4
Alexandre Almeida <lopes.alexandre@eaportal.org>
2024-06-13 23:10:52 +0000

    adicionados as funções do botão de próxima pergunta e de finalizar o quiz

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -93,7 +93,7 @@ function montarPergunta() {
                     </label>
                 </form>
 
-                <button>Enviar</button>
+                <button>Responder</button>
             </section>
         `
 }
@@ -111,7 +111,20 @@ function guardarResposta(evento) {
 }
 
 function validarResposta() {
-    if(resposta === quiz.questions[pergunta-1].answer) {
+    const botaoEnviar = document.querySelector(".alternativas button")
+    botaoEnviar.innerText = "Próxima"
+    botaoEnviar.removeEventListener("click", validarResposta)
+
+    if (pergunta === 10) {
+        botaoEnviar.innerText = "Finalizar"
+        botaoEnviar.addEventListener("click", finalizar)
+    } else {
+        botaoEnviar.addEventListener("click", proximaPergunta)
+    }
+
+
+
+    if (resposta === quiz.questions[pergunta-1].answer) {
         document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "correta")
         pontos = pontos + 1 
     } else {
@@ -119,13 +132,23 @@ function validarResposta() {
         document.querySelector(`label[for='${respostaCorretaId}']`).setAttribute("id", "correta")
     }
 
+    pergunta = pergunta + 1 
+
 }
 
-async function iniciar() {
-    alterarAssunto()
-    await buscarPerguntas()
-    montarPergunta()
+function finalizar() {
+    localStorage.setItem("pontos", pontos)
 
+    window.location.href = "../resultados/resultado.html"
+}
+
+function proximaPergunta() {
+    montarPergunta()
+    adicionarEventoInputs()
+}
+
+
+function adicionarEventoInputs() {
     const inputsResposta = document.querySelectorAll(".alternativas input")
     inputsResposta.forEach(input => {
         addEventListener("click", guardarResposta)
@@ -134,6 +157,13 @@ async function iniciar() {
             respostaCorretaId = input.id
         }
     })
+}
+
+async function iniciar() {
+    alterarAssunto()
+    await buscarPerguntas()
+    montarPergunta()
+    adicionarEventoInputs()
 }
 
 iniciar()
