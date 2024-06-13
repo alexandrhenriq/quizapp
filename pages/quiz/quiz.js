f69b46cc6239b3a73da214e329c5e76fc704dc74
Alexandre Almeida <lopes.alexandre@eaportal.org>
2024-06-13 22:35:24 +0000

    adicionada a função de validar a resposta do usuário

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -9,6 +9,7 @@ let pontos = 0
 let pergunta = 1
 let resposta = ""
 let idInputResposta = ""
+let respostaCorretaId = ""
 
 botaoTema.addEventListener("click", () => {
     trocarTema(body, botaoTema)
@@ -32,13 +33,11 @@ async function buscarPerguntas() {
 
     await fetch(urlDados).then(resposta => resposta.json()).then(dados => {
         dados.quizzes.forEach(dado => {
-            if (dado.title === assunto) {
+            if(dado.title === assunto) {
                 quiz = dado
             }
         })    
     })
-    
-    console.log(quiz)
 }
 
 function montarPergunta() {
@@ -106,6 +105,20 @@ function alterarSinais(texto) {
 function guardarResposta(evento) {
     resposta = evento.target.value
     idInputResposta = evento.target.id
+
+    const botaoEnviar = document.querySelector(".alternativas button")
+    botaoEnviar.addEventListener("click", validarResposta)
+}
+
+function validarResposta() {
+    if(resposta === quiz.questions[pergunta-1].answer) {
+        document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "correta")
+        pontos = pontos + 1 
+    } else {
+        document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "errada")
+        document.querySelector(`label[for='${respostaCorretaId}']`).setAttribute("id", "correta")
+    }
+
 }
 
 async function iniciar() {
@@ -116,6 +129,10 @@ async function iniciar() {
     const inputsResposta = document.querySelectorAll(".alternativas input")
     inputsResposta.forEach(input => {
         addEventListener("click", guardarResposta)
+
+        if(input.value === quiz.questions[pergunta-1].answer) {
+            respostaCorretaId = input.id
+        }
     })
 }
 
