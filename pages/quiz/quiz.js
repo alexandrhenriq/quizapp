0f6935c879be4ceef71e54b066fdfccc8e5fdaad
Alexandre Almeida <lopes.alexandre@eaportal.org>
2024-06-14 10:39:14 +0000

    correção do erro nas respostas do quiz.html

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -58,7 +58,7 @@ function montarPergunta() {
             <section class="alternativas">
                 <form action="">
                     <label for="alternativa_a">
-                        <input type="radio" id="alternativa_a" name="alternativa" value="alterarSinais(quiz.questions[pergunta-1].options[0])">
+                        <input type="radio" id="alternativa_a" name="alternativa" value="${alterarSinais(quiz.questions[pergunta-1].options[0])}">
 
                         <div>
                             <span>A</span>
@@ -67,7 +67,7 @@ function montarPergunta() {
                     </label>
 
                     <label for="alternativa_b">
-                        <input type="radio" id="alternativa_b" name="alternativa" value="alterarSinais(quiz.questions[pergunta-1].options[1])">
+                        <input type="radio" id="alternativa_b" name="alternativa" value="${alterarSinais(quiz.questions[pergunta-1].options[1])}">
 
                         <div>
                             <span>B</span>
@@ -76,7 +76,7 @@ function montarPergunta() {
                     </label>
 
                     <label for="alternativa_c">
-                        <input type="radio" id="alternativa_c" name="alternativa" value="alterarSinais(quiz.questions[pergunta-1].options[2])">
+                        <input type="radio" id="alternativa_c" name="alternativa" value="${alterarSinais(quiz.questions[pergunta-1].options[2])}">
 
                         <div>
                             <span>C</span>
@@ -84,7 +84,7 @@ function montarPergunta() {
                         </div>
                     </label>
                     <label for="alternativa_d">
-                        <input type="radio" id="alternativa_d" name="alternativa" value="alterarSinais(quiz.questions[pergunta-1].options[3])">
+                        <input type="radio" id="alternativa_d" name="alternativa" value="${alterarSinais(quiz.questions[pergunta-1].options[3])}">
 
                         <div>
                             <span>D</span>
@@ -151,7 +151,7 @@ function proximaPergunta() {
 function adicionarEventoInputs() {
     const inputsResposta = document.querySelectorAll(".alternativas input")
     inputsResposta.forEach(input => {
-        addEventListener("click", guardarResposta)
+        input.addEventListener("click", guardarResposta)
 
         if(input.value === quiz.questions[pergunta-1].answer) {
             respostaCorretaId = input.id
